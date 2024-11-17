4c2b236080d54efbb8d594097613d9e988de639a
Soukaina Mourabit <soukainamourabit9@gmail.com>
2024-11-17 21:27:42 +0100

    book class

--- coreTentative/Views/Book.php
+++ coreTentative/Views/Book.php
@@ -10,25 +10,17 @@ class Book extends TemplateView
     /**
      * Handle GET requests.
      */
-    public function get(Request $request): array
+    public function get(Request $request)
     {
-        // Retrieve the route parameter (e.g., /book/:id -> id = 123)
-        $id = $request->attributes->get('id'); 
-
-        // Return data for the template
-        return ['title' => "Book ID: $id"];
+   
     }
 
     /**
      * Handle POST requests (if needed).
      */
-    public function post(Request $request): array
+    public function post(Request $request)
     {
-        // Process POST data from the request
-        $data = $request->request->all();
 
-        // Example response for POST
-        return ['message' => 'POST request received', 'data' => $data];
     }
 
 }
